ad5659f947cffbe4ddf8e4e9b2209197c156bd03
Chris Roth <chrisroth@guestx.com>
2020-02-07 21:56:59 -0600

    setting useable event state in cdm of EventPage.js

--- src/Views/EventPage.js
+++ src/Views/EventPage.js
@@ -5,20 +5,31 @@ import axios from 'axios';
      constructor(props){
          super(props)
          this.state ={
+             id:'',
             venue: {},
-            id:''
+            group: {},
+
          }
      }
 
      componentDidMount(){
          axios.get('https://api.meetup.com/reactjs-dallas/events?&sign=true&photo-host=public&page=1')
          .then(response =>{
-             console.log(response)
+             console.log(response.data[0].id)
+             let res = response.data[0]
+             this.setState({
+                 venue:res.venue,
+                 id: res.id,
+                 group: res.group,
+                 date: res.local_date,
+                 time: res.local_time
+             })
          }).catch(err =>{
              console.log(err)
          })
      }
      render(){
+         console.log(this.state)
          return(
             <div>
             EVENT
